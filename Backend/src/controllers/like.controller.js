e5ee00557ba1a4c4b2fd76ee953685418de38616
Roshan Kumar <veerroshankumar@gmail.com>
2026-09-18 12:33:55 +0530

    feat: implement video like toggle functionality

---
+++ Backend/src/controllers/like.controller.js
@@ -0,0 +1,41 @@
+import mongoose, { mongo } from "mongoose";
+import { Like } from "../models/Like.model.js";
+import {apiError} from "../utils/apiError.js"
+import {apiResponse} from "../utils/apiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+
+const videoLikeToggle =asyncHandler(async(req,res)=>{
+const {videoId}=req.params
+
+if(!mongoose.Types.ObjectId.isValid(videoId)){
+    throw new apiError("400","invalid video id ")
+}
+ 
+const like =await Like.findOne({
+    video:videoId,
+    likedBy:req.user._id
+})
+
+if(like){
+    await Like.findByIdAndDelete(like._id)
+     return res.status(200).json(new apiResponse(201,{isLiked:true},"UnLiked successfully"));
+
+}else{
+   
+    const likeCreated=await Like.create({
+        video:videoId,
+        likedBy:req.user._id
+
+    })
+
+    return res.status(201).json(new apiResponse(201,{isLiked:true},"Liked successfully"))
+}
+
+ 
+
+})
+
+export {
+    videoLikeToggle
+}
